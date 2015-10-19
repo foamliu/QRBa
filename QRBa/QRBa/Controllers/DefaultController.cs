07bde1a4d5a5d27b1ed998b64c83e4666ec4bb18
foamliu <foamliu@yeah.net>
2015-10-19 13:01:03 +0800

    加一个DefaultController

--- QRBa/QRBa/Controllers/DefaultController.cs
+++ QRBa/QRBa/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QRBa.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,12 @@ namespace QRBa.Controllers
 {
     public class DefaultController : ApiController
     {
+        [HttpGet]
+        [Route("{input}")]
+        public HttpResponseMessage OnEvent(string input)
+        {
+            int id = UrlHelper.Code62Decode(input);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
     }
 }
